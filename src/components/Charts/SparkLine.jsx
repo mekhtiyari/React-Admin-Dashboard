2e8ffb4f2564d67d915b75043bed4237c51874bb
mekhtiyari <m.ekhtiyari1992@gmail.com>
2022-12-14 14:06:27 +0330

    Ecommerce page with sparkline and stacked chart completed

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import {
+  SparklineComponent,
+  Inject,
+  SparklineTooltip,
+} from "@syncfusion/ej2-react-charts";
 
-const SparkLine = () => {
-  return <div>SparkLine</div>;
+const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
+  return (
+    <SparklineComponent
+      id={id}
+      height={height}
+      width={width}
+      lineWidth={1}
+      valueType="Numeric"
+      fill={color}
+      border={{ color: currentColor, width: 2 }}
+      tooltipSettings={{
+        visible: true,
+        // eslint-disable-next-line no-template-curly-in-string
+        format: "${x} : data ${y}",
+        trackLineSettings: {
+          visible: true,
+        },
+      }}
+      markerSettings={{
+        visible: ["All"],
+        size: 4.5,
+        fill: "white",
+        border: { color: currentColor, width: 2 },
+      }}
+      dataSource={data}
+      xName="x"
+      yName="y"
+      type={type}
+    >
+      <Inject services={[SparklineTooltip]} />
+    </SparklineComponent>
+  );
 };
 
 export default SparkLine;
